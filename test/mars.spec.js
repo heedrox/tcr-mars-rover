e7e9ed9a235e877e1f03276dba7d40d7787d943c
jmarti <jmarti@theinit.com>
2018-12-30 12:51:08 +0100

    when constructing

--- test/mars.spec.js
+++ test/mars.spec.js
@@ -1,10 +1,12 @@
 const MarsRover = require('../src/mars');
 
 describe('Mars Rover Kata', () => {
-  it('class MarsRover constructs with initial position', () => {
-    const marsRover = new MarsRover({x: 0, y: 0});
+  describe('when constructing', () => {
+    it('class MarsRover constructs with initial position', () => {
+      const marsRover = new MarsRover({x: 0, y: 0});
 
-    expect(marsRover.x).to.equal(0);
-    expect(marsRover.y).to.equal(0);
+      expect(marsRover.x).to.equal(0);
+      expect(marsRover.y).to.equal(0);
+    });
   });
 });
