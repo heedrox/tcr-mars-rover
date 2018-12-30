2218f22a030dc467e5d302ee2957f9a41c577da1
jmarti <jmarti@theinit.com>
2018-12-30 12:51:19 +0100

    ✓ sets initial position

--- test/mars.spec.js
+++ test/mars.spec.js
@@ -2,7 +2,7 @@ const MarsRover = require('../src/mars');
 
 describe('Mars Rover Kata', () => {
   describe('when constructing', () => {
-    it('class MarsRover constructs with initial position', () => {
+    it('sets initial position', () => {
       const marsRover = new MarsRover({x: 0, y: 0});
 
       expect(marsRover.x).to.equal(0);
