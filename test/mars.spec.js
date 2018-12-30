4ac9f49bb9eb8394c55f4878ec6c3e76dc6feb6d
jmarti <jmarti@theinit.com>
2018-12-30 12:55:50 +0100

    when moving   ✓ moves forward

--- test/mars.spec.js
+++ test/mars.spec.js
@@ -3,20 +3,20 @@ const MarsRover = require('../src/mars');
 describe('Mars Rover Kata', () => {
   describe('when constructing', () => {
     it('sets initial position', () => {
-      const marsRover = new MarsRover({x: 0, y: 0});
+      const marsRover = new MarsRover({ x: 0, y: 0 });
 
       expect(marsRover.x).to.equal(0);
       expect(marsRover.y).to.equal(0);
     });
     it('sets facing direction', () => {
-      const marsRover = new MarsRover({x: 0, y: 0}, 'N');
+      const marsRover = new MarsRover({ x: 0, y: 0 }, 'N');
 
       expect(marsRover.direction).to.equal('N');
     });
   });
   describe('when moving', () => {
     it('moves forward', () => {
-      const marsRover = new MarsRover({x: 5, y: 5}, 'N');
+      const marsRover = new MarsRover({ x: 5, y: 5 }, 'N');
 
       marsRover.move(['f']);
 
