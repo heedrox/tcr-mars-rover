5ad5c4e15be203b9bf64bba9fd1c56a2324e1312
jmarti <jmarti@theinit.com>
2018-12-30 15:12:21 +0100

    obstacle detection   ✓ constructs with obstacles

--- test/mars.spec.js
+++ test/mars.spec.js
@@ -5,15 +5,15 @@ const MarsRover = require('../src/mars');
 describe('Mars Rover Kata', () => {
   describe('when constructing', () => {
     it('sets initial position', () => {
-      const marsRover = new MarsRover({ x: 0, y: 0 }, 'N', COMMANDS);
+      const marsRover = new MarsRover({ x: 0, y: 0 }, DIRECTIONS.N, COMMANDS);
 
       expect(marsRover.x).to.equal(0);
       expect(marsRover.y).to.equal(0);
     });
     it('sets facing direction', () => {
-      const marsRover = new MarsRover({ x: 0, y: 0 }, 'N', COMMANDS);
+      const marsRover = new MarsRover({ x: 0, y: 0 }, DIRECTIONS.N, COMMANDS);
 
-      expect(marsRover.direction).to.equal('N');
+      expect(marsRover.direction).to.equal(DIRECTIONS.N);
     });
   });
   describe('when moving', () => {
