35e3c58ea819db5a2e4d686edd8fbba4d957d02d
jmarti <jmarti@theinit.com>
2018-12-30 12:59:46 +0100

    ✓ moves forward - case 5/5 / f

--- test/mars.spec.js
+++ test/mars.spec.js
@@ -15,13 +15,15 @@ describe('Mars Rover Kata', () => {
     });
   });
   describe('when moving', () => {
-    const cases = [ { pos: { x: 5, y: 5 }, command: 'f'} ];
+    const cases = [
+      { pos: { x: 5, y: 5 }, command: 'f', expected: { x: 5, y: 6 } }, ];
     cases.forEach((tcase) => {
       it(`moves forward - case ${tcase.pos.x}/${tcase.pos.y} / ${tcase.command}`, () => {
         const marsRover = new MarsRover({ x: 5, y: 5 }, 'N');
 
         marsRover.move(['f']);
 
+        expect(marsRover.x).to.equal(5);
         expect(marsRover.y).to.equal(6);
       });
     });
